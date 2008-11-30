7791444b819c23533dce311e29564b017a2317cc
Jason Rudolph <github@jasonrudolph.com>
2008-11-29 19:55:00 -0500

    Strip duplicate movies from the search results

--- lib/imdb/imdb_search.rb
+++ lib/imdb/imdb_search.rb
@@ -7,17 +7,24 @@ class ImdbSearch
   end
 
   def movies
-    @movies ||= document.search('a[@href^="/title/tt"]').reject do |element|
-      element.innerHTML.strip_tags.empty?
-    end.map do |element|
-      ImdbMovie.new(element['href'][/\d+/], element.innerHTML.strip_tags.unescape_html)
-    end
+    @movies ||= parse_movies_from_document
   end
 
   private
   
   def document
     @document ||= Hpricot(open("http://www.imdb.com/find?q=#{CGI::escape(query)};s=tt").read)
+  end
+
+  def parse_movies_from_document
+    ids_and_titles = document.search('a[@href^="/title/tt"]').reject do |element|
+      element.innerHTML.strip_tags.empty?
+    end.map do |element|
+      [element['href'][/\d+/], element.innerHTML.strip_tags.unescape_html]
+    end.uniq
+    ids_and_titles.map do |id_and_title|
+      ImdbMovie.new(id_and_title[0], id_and_title[1])
+    end
   end
 
 end
