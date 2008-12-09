04e3275833b5b2a34d8323d3286c6afdbb64711a
Jason Rudolph <github@jasonrudolph.com>
2008-12-08 21:31:59 -0500

    Add support for searches that result in an exact match

--- lib/imdb/imdb_search.rb
+++ lib/imdb/imdb_search.rb
@@ -17,6 +17,16 @@ class ImdbSearch
   end
 
   def parse_movies_from_document
+    exact_match? ? parse_exact_match_search_results : parse_multi_movie_search_results
+  end
+
+  def parse_exact_match_search_results
+    id = document.at("a[@name='poster']")['href'][/\d+$/]
+    title = document.at("h1").innerHTML.split('<span').first.strip.unescape_html rescue nil
+    [ImdbMovie.new(id, title)]
+  end
+
+  def parse_multi_movie_search_results
     ids_and_titles = document.search('a[@href^="/title/tt"]').reject do |element|
       element.innerHTML.strip_tags.empty?
     end.map do |element|
@@ -24,7 +34,11 @@ class ImdbSearch
     end.uniq
     ids_and_titles.map do |id_and_title|
       ImdbMovie.new(id_and_title[0], id_and_title[1])
-    end
+    end    
+  end
+  
+  def exact_match?
+    document.search("title[text()='IMDb Title Search']").empty?
   end
 
 end
