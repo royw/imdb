1dfa9a7f8a4f7c7a80b03a74a0aae715e82421f4
Roy Wright <roy@wright.org>
2009-04-20 02:09:45 -0500

    replaced non-DRY cache_extensions with meta programming

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
-require File.dirname(__FILE__) + '/../lib/imdb'
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require File.dirname(__FILE__) + '/cache_extensions'
+require 'imdb'
 
 $samples_dir = File.dirname(__FILE__) + '/samples'
+
+require 'cache_extensions'
+CacheExtensions.attach_to_read_page_classes($samples_dir)
+
+Spec::Runner.configure do |config|
+
+end
